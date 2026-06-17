2dfa65f5885efc2f80de43221c2421c24389dde8
Cjunwei <xiaorary@gmail.com>
2026-06-17 23:38:10 +0800

    Add files via upload

---
+++ run_realtime.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting Real-Time Detection...
+echo.
+
+"D:\Cjunwei\miniconda3\envs\yolo11m_train\python.exe" "D:\Cjunwei\Document\UniMAP\Course\FYP\Dataset\Finalize\Real-Time.py"
+
+echo.
+echo Python ended.
+pause
